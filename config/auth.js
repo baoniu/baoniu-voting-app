64f5db8ffc19b3bc97462fd761936a6545420fe6
Mr.Chen <688977@qq.com>
2016-06-29 18:52:39 +0800

    update

--- config/auth.js
+++ config/auth.js
@@ -2,6 +2,6 @@ module.exports = {
    githubAuth: {
      clientID: 'e56d8f323d292f6ed04b',
      clientSecret: 'b3beedcf0b49b50f27de6ded1c49ef3aac18a689',
-     callbackURL: 'http://localhost:3000/auth/github/callback',
+     callbackURL: 'https://baoniu-voting-app.herokuapp.com/auth/github/callback',
    }
 };
