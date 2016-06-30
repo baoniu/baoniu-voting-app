45c65ea2ae792b9db292fcfd5d3ac03f627938dd
Mr.Chen <688977@qq.com>
2016-06-30 14:55:35 +0800

    delete

--- config/auth.js
+++
@@ -1,7 +0,0 @@
-module.exports = {
-   githubAuth: {
-     clientID: 'e56d8f323d292f6ed04b',
-     clientSecret: 'b3beedcf0b49b50f27de6ded1c49ef3aac18a689',
-     callbackURL: 'https://baoniu-voting-app.herokuapp.com/auth/github/callback',
-   }
-};
--- config/database.js
+++
@@ -1,3 +0,0 @@
-module.exports = {
-  url: 'mongodb://root:root@ds023052.mlab.com:23052/baoniu',
-};
